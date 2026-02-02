0f9ee386b0309d9755b2bf1d8581dd579713e1f8
Pham Minh Vi <phamminhvi2280603655@gmail.com>
2026-02-02 15:39:07 +0700

    Remove root level files - keep only Baitap3 folder

--- src/main/java/com/example/Bai2/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Bai2.repository;
-
-import com.example.Bai2.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
